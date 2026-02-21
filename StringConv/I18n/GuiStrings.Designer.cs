42848623485c8cc34160c4df17f25bc260809c26
detached64 <169419571+detached64@users.noreply.github.com>
2026-02-21 09:44:34 +0800

    feat: add Unicode string converter

--- StringConv/I18n/GuiStrings.Designer.cs
+++ StringConv/I18n/GuiStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace StringConv.I18n {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class GuiStrings {
@@ -255,6 +255,15 @@ namespace StringConv.I18n {
         public static string SourceURL {
             get {
                 return ResourceManager.GetString("SourceURL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 Unicode String 的本地化字符串。
+        /// </summary>
+        public static string UnicodeString {
+            get {
+                return ResourceManager.GetString("UnicodeString", resourceCulture);
             }
         }
     }
--- StringConv/Models/Converters/DataEncodingConverter.cs
+++ StringConv/Models/Converters/DataEncodingConverter.cs
@@ -1,5 +1,7 @@
 using StringConv.I18n;
 using System;
+using System.Globalization;
+using System.IO;
 using System.Text;
 
 namespace StringConv.Models.Converters;
@@ -50,6 +52,41 @@ internal sealed class HexStringConverter : DataEncodingConverter
             {
                 sb.Append(c);
             }
+        }
+        return sb.ToString();
+    }
+}
+
+internal sealed class UnicodeStringConverter : DataEncodingConverter
+{
+    public override string Name => GuiStrings.UnicodeString;
+
+    public override byte[] FromString(string input)
+    {
+        string[] parts = input.Split([@"\u"], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        byte[] bytes = new byte[parts.Length * 2];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i].Length != 4 || !ushort.TryParse(parts[i], NumberStyles.HexNumber, null, out ushort codePoint))
+            {
+                throw new InvalidDataException();
+            }
+            bytes[i * 2] = (byte)(codePoint & 0xFF);
+            bytes[i * 2 + 1] = (byte)(codePoint >> 8);
+        }
+        return bytes;
+    }
+
+    public override string ToString(byte[] input)
+    {
+        if (input.Length % 2 != 0)
+        {
+            throw new InvalidDataException();
+        }
+        StringBuilder sb = new();
+        for (int i = 0; i < input.Length; i += 2)
+        {
+            sb.Append($@"\u{(ushort)(input[i] | (input[i + 1] << 8)):X4}");
         }
         return sb.ToString();
     }
