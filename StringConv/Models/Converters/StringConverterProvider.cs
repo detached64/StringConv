d04030e2eeb78e03f53f388e877dec678722f411
detached64 <169419571+detached64@users.noreply.github.com>
2026-02-24 19:05:10 +0800

    feat: use dictionary for faster converter lookup by id

--- StringConv/Models/Converters/StringConverterProvider.cs
+++ StringConv/Models/Converters/StringConverterProvider.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StringConv.Models.Converters;
 
 internal static partial class StringConverterProvider
 {
     private static readonly Lazy<List<StringConverter>> _converters = new(InitConverters);
+    private static readonly Lazy<Dictionary<string, StringConverter>> _converterMap = new(() => _converters.Value.ToDictionary(c => c.Id));
     private static List<StringConverter> InitConverters() => [.. LoadConvertersGenerated()];
     public static List<StringConverter> Converters => _converters.Value;
 
     public static StringConverter Find(string id)
     {
-        return Converters.Find(c => c.Id == id);
+        return _converterMap.Value.GetValueOrDefault(id);
     }
 }
