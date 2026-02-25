144899748cdefa11d3743761f8f000433776ac2a
detached64 <169419571+detached64@users.noreply.github.com>
2026-02-25 16:11:08 +0800

    refactor: remove redundant '== true' in CanPinMultiple property

--- StringConv/ViewModels/MainViewModel.cs
+++ StringConv/ViewModels/MainViewModel.cs
@@ -306,7 +306,7 @@ internal partial class MainViewModel : ViewModelBase
         !_settingsService.Settings.PinnedConverterIds.Contains(SelectedConverter.Id);
 
     private bool CanPinMultiple => SelectedConverters.OfType<StringConverter>()
-        .Any(c => c.CanConvert && !_settingsService.Settings.PinnedConverterIds.Contains(c.Id)) == true;
+        .Any(c => c.CanConvert && !_settingsService.Settings.PinnedConverterIds.Contains(c.Id));
 
     partial void OnSelectedConverterChanged(StringConverter value)
     {
